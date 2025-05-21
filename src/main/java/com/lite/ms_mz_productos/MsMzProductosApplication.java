ff8281c9ba1f7ee863b2f7c7a883c83c4fde72b3
Antoni Tito <X202088998@cibertec.edu.pe>
2025-05-21 18:48:16 -0500

    Inicializamos solo con spring web

---
+++ src/main/java/com/lite/ms_mz_productos/MsMzProductosApplication.java
@@ -0,0 +1,13 @@
+package com.lite.ms_mz_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMzProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMzProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lite/ms_mz_productos/MsMzProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lite.ms_mz_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMzProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
