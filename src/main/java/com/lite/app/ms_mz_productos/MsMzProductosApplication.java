5d6fb21ee76c37a938868af67aca2c163ace4914
Antoni Tito <X202088998@cibertec.edu.pe>
2025-06-03 01:46:42 -0500

    jeje

--- src/main/java/com/lite/app/ms_mz_productos/MsMzProductosApplication.java
+++ src/main/java/com/lite/app/ms_mz_productos/MsMzProductosApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsMzProductosApplication {
-jhgghjjhg
+
 	public static void main(String[] args) {
 		SpringApplication.run(MsMzProductosApplication.class, args);
+		System.out.println("Hola");
 	}
 
 }
