662071020bf28370c00d46f720af60145aed2685
Antoni Tito <X202088998@cibertec.edu.pe>
2025-06-03 01:37:22 -0500

    falla

--- src/main/java/com/lite/app/ms_mz_productos/MsMzProductosApplication.java
+++ src/main/java/com/lite/app/ms_mz_productos/MsMzProductosApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsMzProductosApplication {
-
+jhgghjjhg
 	public static void main(String[] args) {
 		SpringApplication.run(MsMzProductosApplication.class, args);
 	}
